92055925916235138b12cc0b54d6e1af7bac5296
Smith <chendingchao1@126.com>
2017-04-17 10:01:36 +0800

    developing

---
+++ src/main/java/org/config/ConfigConstant.java
@@ -0,0 +1,12 @@
+package org.config;
+
+/**
+ * Created by Smith on 2017/4/16.
+ */
+public interface ConfigConstant {
+    String CONFIG_FILE = "smart.properties";
+
+    String APP_BASE_PACKAGE = "smart.app.base_package";
+    String APP_JSP_PATH = "smart.app.jsp_path";
+    String APP_ASSET_PATH = "smart.app.asset_path";
+}
---
+++ src/main/java/org/config/ConfigHelper.java
@@ -0,0 +1,23 @@
+package org.config;
+
+import java.util.Properties;
+
+/**
+ * Created by Smith on 2017/4/16.
+ */
+public class ConfigHelper {
+    private static final Properties CONFIG_PROPS = PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+
+    public static String getAppBasePackage() {
+        return CONFIG_PROPS.getProperty(ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+    public static String getAppJspPath() {
+        return CONFIG_PROPS.getProperty(ConfigConstant.APP_JSP_PATH);
+    }
+
+    public static String getAppAssetPath() {
+        return CONFIG_PROPS.getProperty(ConfigConstant.APP_ASSET_PATH);
+    }
+
+}
